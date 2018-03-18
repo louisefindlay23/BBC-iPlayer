04f48901f87e6d9c9d1a8d00a388594b01cba8e9
Louise <30032093@nescol.ac.uk>
2018-03-18 22:13:25 +0000

    Carousel complete

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	'use strict';
+  $('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: false,
+  });
+});
