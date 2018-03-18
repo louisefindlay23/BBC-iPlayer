cacc0f3170a40ceb05f6f970e2e1a20cee869387
Louise <30032093@nescol.ac.uk>
2018-03-18 19:02:12 +0000

    Working nav menu - show/hide

--- js/navigation.js
+++ js/navigation.js
@@ -4,20 +4,38 @@ $(document).ready(function(){
 	'use strict';
     $("#navhome").click(function(){
         $("#home").show();
-		$(".hero").show();
-		$(".carousel").show();
+		$("#tvshow").hide();
+		$("#categories").hide();
+		$("#search").hide();
+		$("#profile").hide();
+		$("#faq").hide();
     });
 	
 	$("#navcategory").click(function(){
         $("#categories").show();
+		$("#home").hide();
+		$("#tvshow").hide();
+		$("#search").hide();
+		$("#profile").hide();
+		$("#faq").hide();
     });
 	
 	$("#navsearch").click(function(){
         $("#search").show();
+		$("#home").hide();
+		$("#tvshow").hide();
+		$("#categories").hide();
+		$("#profile").hide();
+		$("#faq").hide();
     });
 	
 	$("#navprofile").click(function(){
         $("#profile").show();
+		$("#home").hide();
+		$("#tvshow").hide();
+		$("#categories").hide();
+		$("#search").hide();
+		$("#faq").hide();
     });
 	
 	// Add more jquery
