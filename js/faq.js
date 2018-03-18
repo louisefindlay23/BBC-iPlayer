28367e77c4a373ed736a7acff64b32228752a99c
gfrcsd <gfrcsd@gmail.com>
2018-03-18 03:10:37 +0000

    Minor ID name change

--- js/faq.js
+++ js/faq.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     $("#q1").click(function(){
-        $("#r1").slideToggle();
+        $("#a1").slideToggle();
         $("#q1").toggleClass("q-active")
     });
     $("#q2").click(function(){
-        $("#r2").slideToggle();
+        $("#a2").slideToggle();
         $("#q2").toggleClass("q-active")
     });
     $("#q3").click(function(){
-        $("#r3").slideToggle();
+        $("#a3").slideToggle();
         $("#q3").toggleClass("q-active")
     });
     $("#q4").click(function(){
-        $("#r4").slideToggle();
+        $("#a4").slideToggle();
         $("#q4").toggleClass("q-active")
     });
 });
