cbacb6cf046ccfc03db05d5f51f73c221787ac95
gfrcsd <gfrcsd@gmail.com>
2018-03-18 21:04:59 +0000

    Shortened navigation.js with multiple selectors

--- js/navigation.js
+++ js/navigation.js
@@ -4,44 +4,28 @@ $(document).ready(function(){
 	'use strict';
     $("#navhome").click(function(){
         $("#home").show();
-		$("#tvshow").hide();
-		$("#categories").hide();
-		$("#search").hide();
-		$("#profile").hide();
-		$("#faq").hide();
+		$("#faq, #search, #categories, #tvshow, #settings, #profile").hide();
 		$("#navhome").addClass("element-active");
 		$("#navcategory, #navsearch, #navprofile, #settings-icon").removeClass("element-active");
     });
 	
 	$("#navcategory").click(function(){
         $("#categories").show();
-		$("#home").hide();
-		$("#tvshow").hide();
-		$("#search").hide();
-		$("#profile").hide();
-		$("#faq").hide();
+		$("#home, #faq, #search, #tvshow, #settings, #profile").hide();
 		$("#navcategory").addClass("element-active");
 		$("#navhome, #navsearch, #navprofile, #settings-icon").removeClass("element-active");
     });
 	
 	$("#navsearch").click(function(){
         $("#search").show();
-		$("#home").hide();
-		$("#tvshow").hide();
-		$("#categories").hide();
-		$("#profile").hide();
-		$("#faq").hide();
+		$("#home, #faq, #categories, #tvshow, #settings, #profile").hide();
 		$("#navsearch").addClass("element-active");
 		$("#navhome, #navcategory, #navprofile, #settings-icon").removeClass("element-active");
     });
 	
 	$("#navprofile").click(function(){
         $("#profile").show();
-		$("#home").hide();
-		$("#tvshow").hide();
-		$("#categories").hide();
-		$("#search").hide();
-		$("#faq").hide();
+		$("#home, #faq, #search, #categories, #tvshow, #settings").hide();
 		$("#navprofile").addClass("element-active");
 		$("#navhome, #navsearch, #navcategory, #settings-icon").removeClass("element-active");
 	});
