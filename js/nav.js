403d47e7b737228f973d563faa990c77c84d4746
gfrcsd <gfrcsd@gmail.com>
2018-03-18 00:28:08 +0000

    Clean up extra JS in header animation, add padding bottom to body

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,3 @@
 var myElement = document.querySelector("header");
 var headroom  = new Headroom(myElement);
-headroom.init(); 
-
-var headroom = new Headroom(elem, {
-    "offset": 0,
-    "tolerance": 0,
-    "classes": {
-      "initial": "animated",
-      "pinned": "slideDown",
-      "unpinned": "slideUp"
-    }
-  });
-  headroom.init();
-  
-  // to destroy
-  headroom.destroy();
+headroom.init();
