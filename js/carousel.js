287365043381bf13c9925cae3462b3bd71ace432
Louise <30032093@nescol.ac.uk>
2018-03-19 17:07:26 +0000

    Carousel actually complete (#23)
    * Fixed wraparound. Carousel now infinitely scrolls.
    
    * Carousel contained properly. Stops at last image.
    
    * Removed margin-right .carousel-cell
    
    * Scrolling right but see only one picture at a time.
    
    * Quick fix carousel
    
    * Add CSS background images to carousel with logo embeded on top
    
    * Change logo max-height on carousel
    
    * Removed an extra CSS property

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,9 @@
 // JavaScript Document
 
-$(document).ready(function(){
-	'use strict';
-  $('.main-carousel').flickity({
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
-  wrapAround: true,
-});
+  freeScroll: true,
 });
