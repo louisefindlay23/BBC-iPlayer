352c7578e00c8a5203e268bb2d5b11c0f1e96de9
gfrcsd <gfrcsd@gmail.com>
2018-03-19 12:43:41 +0000

    Change arrows directions on FAQ

--- js/faq.js
+++ js/faq.js
@@ -2,21 +2,21 @@ $(document).ready(function(){
     $("#q1").click(function(){
         $("#a1").slideToggle();
         $("#q1").toggleClass("element-active");
-        $("#s1").toggleClass("fa-caret-down, fa-caret-up");
+        $("#s1").toggleClass("fa-caret-right fa-caret-down");
     });
     $("#q2").click(function(){
         $("#a2").slideToggle();
         $("#q2").toggleClass("element-active");
-        $("#s2").toggleClass("fa-caret-down, fa-caret-up");
+        $("#s2").toggleClass("fa-caret-right fa-caret-down");
     });
     $("#q3").click(function(){
         $("#a3").slideToggle();
         $("#q3").toggleClass("element-active");
-        $("#s3").toggleClass("fa-caret-down, fa-caret-up");
+        $("#s3").toggleClass("fa-caret-right fa-caret-down");
     });
     $("#q4").click(function(){
         $("#a4").slideToggle();
         $("#q4").toggleClass("element-active");
-        $("#s4").toggleClass("fa-caret-down, fa-caret-up");
+        $("#s4").toggleClass("fa-caret-right fa-caret-down");
     });
 });
