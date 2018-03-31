ea6b193c066f38fd4c60a188db0d3b9754d4f745
Joey <23112101+gfrcsd@users.noreply.github.com>
2018-03-31 01:40:45 +0100

    Organise *nearly* all JavaScript into a single file
    Remove all redudant JS files and links as a result

--- js/header.js
+++
@@ -1,6 +0,0 @@
-var myElement = document.querySelector("header");
-var headroom = new Headroom(myElement, {
-    "offset": 65,
-    "tolerance": 3
-  });
-headroom.init();
--- js/profile-picture.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#trigger-zoom").click(function () { 
-        $("#picture-zoom").fadeIn("fast");
-    });
-    $("#picture-zoom").click(function() { 
-        $("#picture-zoom").fadeOut("fast");
-    });
-});
